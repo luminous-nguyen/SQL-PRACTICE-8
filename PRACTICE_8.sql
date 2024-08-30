b50ddb6a6592fbc7561f0385ac4612f8acdc9f81
luminous-nguyen <127753982+luminous-nguyen@users.noreply.github.com>
2024-08-30 23:52:34 +0700

    Update PRACTICE_8.sql

--- PRACTICE_8.sql
+++ PRACTICE_8.sql
@@ -39,4 +39,29 @@ SELECT ID,
            ELSE COALESCE(LEAD(STUDENT) OVER(ORDER BY ID), STUDENT)
        END AS STUDENT
 FROM SEAT
+-- Ex 4
+--Ex 4
 
+SELECT visited_on,
+       amount,
+       average_amount
+FROM (    
+
+       SELECT visited_on,
+              ROUND(
+                    SUM(amount) OVER(ORDER BY visited_on
+                                     ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),2
+                   ) AS amount,
+              ROUND(
+                    AVG(amount) OVER(ORDER BY visited_on
+                              ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),2
+                   ) AS average_amount      
+       FROM(      
+             SELECT visited_on,
+                    SUM(amount) AS amount,
+                    AVG(amount) AS average_amount
+             FROM Customer
+             GROUP BY visited_on
+           ) AS CTE
+     ) AS CTE2
+WHERE visited_on >= '2019-01-07'
